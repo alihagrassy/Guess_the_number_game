8befb6f2c4f914c89cf35c854a0512bd25513fe1
Ali waleed Ali <82417794+alimistro1234@users.noreply.github.com>
2022-09-06 11:53:01 +0200

    Add files via upload

---
+++ the_game.py
@@ -0,0 +1,23 @@
+import getpass
+def guess_the_number(x,guess):
+    if guess!=0:
+        y=int(input('Choose the chosen  number'))
+        if x==y:
+            print("Great Job you have the right number")
+            return
+        elif x<y:
+            print("The number is  biger")
+            guess-=1
+            guess_the_number(x,guess)
+        elif x>y:
+            print("The number is smaller")
+            guess-=1
+            guess_the_number(x,guess)
+    else:
+        print("You are out of guesses  the right answer is:",x)
+x=int(getpass.getpass(" Choose the Wanted Number: "))
+
+g=int(input(" Choose the number of Avilable gusses"))
+guess_the_number(x,g)
+
+
